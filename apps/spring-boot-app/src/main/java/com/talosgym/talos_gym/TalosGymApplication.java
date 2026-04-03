214d97d8fa04c9ab7ea7dcef1e016761a3389328
Alaattin Düzdere <aleykedu@gmail.com>
2026-04-03 12:59:20 +0300

    chore(config): update infrastructure and redis configurations
    - Add RedisConfig and SecurityProperties classes.
    - Update application.properties and .env variables.
    - Update pom.xml for new dependencies.
    - Modify docker-compose files for local development environment.

--- apps/spring-boot-app/src/main/java/com/talosgym/talos_gym/TalosGymApplication.java
+++ apps/spring-boot-app/src/main/java/com/talosgym/talos_gym/TalosGymApplication.java
@@ -2,12 +2,12 @@ package com.talosgym.talos_gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TalosGymApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TalosGymApplication.class, args);
 	}
-
 }
