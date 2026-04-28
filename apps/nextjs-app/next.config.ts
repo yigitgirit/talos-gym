504721a5349b6d8de689f47811ac7ab5c1981676
yigit-7 <yigit.girit@hotmail.com>
2026-04-28 16:59:52 +0300

    feat(nextjs): enable standalone output, enhance TypeScript configurations, and update dependencies
    - Set Next.js output to "standalone" for easier deployment.
    - Enforce stricter TypeScript settings with `noImplicitAny`, `noImplicitThis`, and `useUnknownInCatchVariables`.
    - Add various frontend dependencies, including `react-query`, `react-hook-form`, `zustand`, and `date-fns`.

--- apps/nextjs-app/next.config.ts
+++ apps/nextjs-app/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'm.media-amazon.com',
+    //         },
+    //     ],
+    // },
 };
 
 export default nextConfig;
